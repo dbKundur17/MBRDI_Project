fbab592cb699d7b0f9e2bb69e8fa0d96f8c45cab
dbKundur17 <71865142+dbKundur17@users.noreply.github.com>
2022-06-26 13:39:41 +0530

    Create LoginFormApplication.java

---
+++ mbrdi.cars.loginform/LoginFormApplication.java
@@ -0,0 +1,14 @@
+package mbrdi.cars.loginform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginFormApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LoginFormApplication.class, args);
+	}
+
+}
